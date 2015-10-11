543eeacfc5d028638985145d6aef21d6ec97dcf7
paktinat <saeid.paktinat@cern.ch>
2015-10-11 19:32:51 +0330

    ispell and more.

--- notes/SUS-14-022/trunk/StatisticsFig/MT2muTau_tauMTgt200_DDFake.C
+++ notes/SUS-14-022/trunk/StatisticsFig/MT2muTau_tauMTgt200_DDFake.C
@@ -117,8 +117,8 @@
    hAllMC__QCD->GetZaxis()->SetLabelSize(0.035);
    hAllMC__QCD->GetZaxis()->SetTitleSize(0.035);
    hAllMC__QCD->GetZaxis()->SetTitleFont(42);
-   h_stack->Add(hAllMC__QCD,"");
-   h__MC->Add(hAllMC__QCD);
+//    h_stack->Add(hAllMC__QCD,"");
+//    h__MC->Add(hAllMC__QCD);
 
    Double_t xAxis2[121] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120}; 
    
@@ -268,8 +268,8 @@
    hAllMC__Wjets->GetZaxis()->SetLabelSize(0.035);
    hAllMC__Wjets->GetZaxis()->SetTitleSize(0.035);
    hAllMC__Wjets->GetZaxis()->SetTitleFont(42);
-   h_stack->Add(hAllMC__Wjets,"");
-   h__MC->Add(hAllMC__Wjets);
+//    h_stack->Add(hAllMC__Wjets,"");
+//    h__MC->Add(hAllMC__Wjets);
    Double_t xAxis3[121] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120}; 
    
    TH1D *hAllMC1__Zjets = new TH1D("hAllMC1__Zjets","",120, xAxis3);
@@ -1143,7 +1143,7 @@ tex->SetNDC();
    tex->SetLineWidth(2);
    tex->Draw();
    
-      tex = new TLatex(0.25,0.850,"CMS Preliminary");
+      tex = new TLatex(0.25,0.850,"CMS Preliminary     #mu#tau");
 tex->SetNDC();
    tex->SetTextSize(0.05);
    tex->SetLineWidth(2);
