0b2064788716e10f74e786ebee95caac22651f94
Esmaeel Eskandari <esi.esk@gmail.com>
2016-07-15 01:45:42 +0430

    References are modified

--- papers/SUS-14-022/trunk/Introductionfigs/Compile.sh
+++ papers/SUS-14-022/trunk/Introductionfigs/Compile.sh
@@ -2,8 +2,11 @@ pdflatex Diagrams.tex
 for f in *.mp; do mpost $f; done
 pdflatex Diagrams.tex
 
-pdfcrop --margins '1 5 5 -365' Diagrams.pdf TChipmSlepSnu.pdf
-pdfcrop --margins '1 -240 -110 -210' Diagrams.pdf TSlepSlep.pdf
+#pdfcrop --margins '1 5 5 -365' Diagrams.pdf TChipmSlepSnu.pdf
+#pdfcrop --margins '1 -240 -110 -210' Diagrams.pdf TSlepSlep.pdf
+
+pdfcrop  Diagrams.pdf TChipmSlepSnu.pdf
+pdfcrop  Diagrams.pdf TSlepSlep.pdf
 
 rm -rf *.log
 rm -rf *.mp
